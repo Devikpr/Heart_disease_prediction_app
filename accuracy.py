9222ada90c4392bb336c66716899369368c0bf8f
DEVIKA P R <devikarajrajan@gmail.com>
2025-03-30 00:06:15 +0530

    Create accuracy.py

---
+++ accuracy.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+# Ensure the file has a .csv extension
+df = pd.read_csv(r"C:\Users\Devika\Desktop\project\dataset.csv")  
+
+# Display the first few rows
+print(df.head())
+#Assignining dependent and independent variables.
+x=df.iloc[:,:-1]
+print(x.head())
+
+y=df.iloc[:,-1]
+print(y.head())
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test= train_test_split(x,y,test_size=0.2)
+
+from sklearn.linear_model import LogisticRegression
+model=LogisticRegression()
+
+#fiting the the line
+model.fit(X_train,Y_train)
+
+#prediction using x_test
+
+y_pred=model.predict(X_test)
+
+#checking the accuracy
+from sklearn.metrics import accuracy_score
+print("Accuracy is",accuracy_score(Y_test,y_pred))
+
+new_array=np.array([[40,1,2,140,289,0,0,172,0,0,1]])
+new_pred=model.predict(new_array)
+print(new_pred)
